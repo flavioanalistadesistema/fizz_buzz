031276f36b192e9df8942f8c96af99c5c5cf3c6d
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-08-07 18:46:22 -0300

    Refactore: smaill refactore

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,20 +5,24 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
+  defp handle_file_read({:ok, result}) do
+    result =
     result
     |> String.split(",")
     |> Enum.map(&convert_and_valuate_numbers/1)
-  end
-  def handle_file_read({:error, msg}), do: "Não foi possivel ler o arquivo. Menssage #{msg}"
-
-  def convert_and_valuate_numbers(elem) do
-    number = String.to_integer(elem)
-    evaluate_number(number)
+    {:ok, result}
   end
 
-  def evaluate_number(number) when rem(number, 3) === 0 and rem(number, 5) === 0,  do: :fizzbuzz
-  def evaluate_number(number) when rem(number, 5) === 0,  do: :buzz
-  def evaluate_number(number) when rem(number, 3) === 0,  do: :fizz
-  def evaluate_number(number), do: number
+  defp handle_file_read({:error, msg}), do: {:error , "Não foi possivel ler o arquivo. Menssage #{msg}"}
+
+  defp convert_and_valuate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_number()
+  end
+
+  defp evaluate_number(number) when rem(number, 3) === 0 and rem(number, 5) === 0,  do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 5) === 0,  do: :buzz
+  defp evaluate_number(number) when rem(number, 3) === 0,  do: :fizz
+  defp evaluate_number(number), do: number
 end
