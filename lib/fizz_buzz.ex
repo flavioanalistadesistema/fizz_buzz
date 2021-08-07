55ca7c757e56d8cf5925c6e0281a991e83d22440
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-08-07 18:09:06 -0300

    refactore: refactore with guards

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -8,7 +8,17 @@ defmodule FizzBuzz do
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_valuate_numbers/1)
   end
   def handle_file_read({:error, msg}), do: "Não foi possivel ler o arquivo. Menssage #{msg}"
+
+  def convert_and_valuate_numbers(elem) do
+    number = String.to_integer(elem)
+    evaluate_number(number)
+  end
+
+  def evaluate_number(number) when rem(number, 3) === 0 and rem(number, 5) === 0,  do: :fizzbuzz
+  def evaluate_number(number) when rem(number, 5) === 0,  do: :buzz
+  def evaluate_number(number) when rem(number, 3) === 0,  do: :fizz
+  def evaluate_number(number), do: number
 end
